aaf7db4031eb2f8e1d51152843dcb9ee4afb97a8
emanuelnascente <emanuelalvorada@gmail.com>
2023-08-07 22:54:05 -0300

    alterações de rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ArearestritaView from "../views/AreaRestritaView.vue";
+import Register from "../views/Register.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
 			path: "/",
 			name: "home",
 			component: HomeView,
+		},
+		{
+			path: "/registre-se",
+			name: "Register",
+			component: Register,
 		},
 		{
 			path: "/clientes",
