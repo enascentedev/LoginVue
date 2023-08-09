02e73049d8d1f1a2d06cff0b3a0612cb6e8f5832
emanuelnascente <emanuelalvorada@gmail.com>
2023-08-08 23:05:03 -0300

    instalação de plug-in animação tailwind

--- src/components/Access.vue
+++ src/components/Access.vue
@@ -37,26 +37,31 @@ export default {
 </script>
 
 <template>
-	<div class="flex justify-center items-center py-5">
-		<div class="mockup-phone px-5">
+	<div class="flex justify-end py-5 bg-gray-900">
+		<div
+			class="mockup-phone px-5 bg-gray-900 animate-wiggle animate-infinite animate-ease-linear">
 			<div class="camera"></div>
 			<div class="display">
-				<div class="artboard artboard-demo phone-1">
-					<form @submit.prevent="fazerLogin">
-						<input
-							v-model="usuario.email"
-							type="text"
-							placeholder="digite seu email"
-							class="input input-bordered input-lg w-3/4 max-w-xs mx-5 my-3" />
-						<input
-							v-model="usuario.senha"
-							type="password"
-							placeholder="digite sua senha"
-							class="input input-bordered input-lg w-3/4 max-w-xs mx-5 my-3" />
-						<button @submit.prevent class="btn mx-5">Entrar</button>
-						<router-link to="/registre-se"
-							><button class="btn mx-5">cadastra-se</button></router-link
-						>
+				<div class="artboard artboard-demo phone-1 bg-blue-700">
+					<form class="bg-blue-700" @submit.prevent="fazerLogin">
+						<div class="flex flex-col items-center">
+							<input
+								v-model="usuario.email"
+								type="text"
+								placeholder="digite seu email"
+								class="input input-bordered input-lg w-3/4 max-w-xs mx-5 my-3" />
+							<input
+								v-model="usuario.senha"
+								type="password"
+								placeholder="digite sua senha"
+								class="input input-bordered input-lg w-3/4 max-w-xs mx-5 my-3" />
+						</div>
+						<div class="flex justify-center">
+							<button @submit.prevent class="btn mx-2">Entrar</button>
+							<router-link to="/registre-se"
+								><button class="btn mx-2">cadastra-se</button></router-link
+							>
+						</div>
 					</form>
 				</div>
 			</div>
---
+++ src/components/Marketing.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+	name: "Marketing",
+};
+</script>
+
+<template>
+	<div
+		style="
+			background-image: url('https://uvn-brightspot.s3.amazonaws.com/assets/vixpt/t/tecnologia-iot-1400x900-1119.jpg');
+			background-size: cover;
+		"></div>
+</template>
